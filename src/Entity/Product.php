587042ad71d84c5d610985d05f87c1de059df556
Bella <text_me2@protonmail.com>
2019-02-12 22:23:54 +0100

    settings in config folder updated & entity/controller for products created

---
+++ src/Entity/Product.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $productName;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productPrice;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $productDescription;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $productBrand;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProductName(): ?string
+    {
+        return $this->productName;
+    }
+
+    public function setProductName(string $productName): self
+    {
+        $this->productName = $productName;
+
+        return $this;
+    }
+
+    public function getProductPrice(): ?int
+    {
+        return $this->productPrice;
+    }
+
+    public function setProductPrice(int $productPrice): self
+    {
+        $this->productPrice = $productPrice;
+
+        return $this;
+    }
+
+    public function getProductDescription(): ?string
+    {
+        return $this->productDescription;
+    }
+
+    public function setProductDescription(string $productDescription): self
+    {
+        $this->productDescription = $productDescription;
+
+        return $this;
+    }
+
+    public function getProductBrand(): ?string
+    {
+        return $this->productBrand;
+    }
+
+    public function setProductBrand(string $productBrand): self
+    {
+        $this->productBrand = $productBrand;
+
+        return $this;
+    }
+}
