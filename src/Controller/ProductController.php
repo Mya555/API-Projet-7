fb5b5a8e49a58d8e09d7aa9160c864c73571e2fb
Bella <text_me2@protonmail.com>
2019-02-18 15:51:52 +0100

    controller & repository folders updated / representation class added

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use PagerFanta\Pagerfanta;
 use App\Repository\ProductRepository;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Request\ParamFetcherInterface;
 
 class ProductController extends AbstractController
 {
@@ -27,10 +30,43 @@ class ProductController extends AbstractController
 
     /**
      * @Rest\Get("/product", name="list_product")
+     * @Rest\QueryParam(
+     *     name="keyword",
+     *     requirements="[a-zA-Z0-9]",
+     *     nullable=true,
+     *     description="Le mot clé a chercher."
+     * )
+     * @Rest\QueryParam(
+     *     name="order",
+     *     requirements="asc|desc",
+     *     default="asc",
+     *     description="Ordre croissant ou décroissant"
+     * )
+     * @Rest\QueryParam(
+     *     name="limit",
+     *     requirements="\d+",
+     *     default="10",
+     *     description="Le nombre maximale de produit par page."
+     * )
+     * @Rest\QueryParam(
+     *     name="offset",
+     *     requirements="\d+",
+     *     default="1",
+     *     description="L'index de l'élément par lequel on commence"
+     * )
      * @Rest\View
+     * @param ParamFetcherInterface $paramFetcher
+     * @return
      */
-    public function listProduct(){
-        $product = $this->getDoctrine()->getRepository(Product::class)->findAll();
-        return $product;
+    public function listProduct(ParamFetcherInterface $paramFetcher){
+        //$product = $this->getDoctrine()->getRepository(Product::class)->findAll();
+        //return $product;
+        $pager = $this->getDoctrine()->getRepository(Product::class)->search(
+            $paramFetcher->get('keyword'),
+            $paramFetcher->get('order'),
+            $paramFetcher->get('limit'),
+            $paramFetcher->get('offset')
+        );
+        return $pager->getCurrentPageResults();
     }
 }
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -3,48 +3,25 @@
 namespace App\Repository;
 
 use App\Entity\Product;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use PagerFanta\Pagerfanta;
 
-/**
- * @method Product|null find($id, $lockMode = null, $lockVersion = null)
- * @method Product|null findOneBy(array $criteria, array $orderBy = null)
- * @method Product[]    findAll()
- * @method Product[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
-class ProductRepository extends ServiceEntityRepository
+
+class ProductRepository extends AbstractRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function search($term, $order = 'asc', $limit = 20, $offset = 0)
     {
-        parent::__construct($registry, Product::class);
-    }
-
-    // /**
-    //  * @return Product[] Returns an array of Product objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
+        $qb = $this
+            ->createQueryBuilder('a')
+            ->select('a')
+            ->orderBy('a.productName', $order)
         ;
+        if ($term) {
+            $qb
+                ->where('a.productName LIKE ?1')
+                ->setParameter(1, '%'.$term.'%')
+            ;
+        }
+        return $this->paginate($qb, $limit, $offset);
     }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Product
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
