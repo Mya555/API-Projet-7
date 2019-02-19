c114576c1785e1f821da581f99bb227e9146b3f0
Bella <text_me2@protonmail.com>
2019-02-19 21:51:18 +0100

    user authentification added

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,87 +1,51 @@
 <?php
-
 namespace App\Entity;
-
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Component\Security\Core\User\UserInterface;
 /**
  * @ORM\Table(name="users")
- * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Entity
  */
-class User
+class User implements UserInterface
 {
     /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue(strategy="AUTO")
      * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
-
     /**
-     * @ORM\Column(type="string", length=255, unique=true)
+     * @ORM\Column(type="string", length=25, unique=true)
      */
     private $username;
-
     /**
      * @ORM\Column(type="string", length=500)
      */
     private $password;
-
     /**
      * @ORM\Column(name="is_active", type="boolean")
      */
     private $isActive;
-
     public function __construct($username)
     {
         $this->isActive = true;
         $this->username = $username;
     }
-
+    public function getUsername()
+    {
+        return $this->username;
+    }
     public function getSalt()
     {
         return null;
     }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getUsername(): ?string
-    {
-        return $this->username;
-    }
-
-    public function setUsername(string $username): self
-    {
-        $this->username = $username;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
+    public function getPassword()
     {
         return $this->password;
     }
-
-    public function setPassword(string $password): self
+    public function setPassword($password)
     {
         $this->password = $password;
-
-        return $this;
-    }
-
-    public function getIsActive(): ?bool
-    {
-        return $this->isActive;
-    }
-
-    public function setIsActive(bool $isActive): self
-    {
-        $this->isActive = $isActive;
-
-        return $this;
     }
     public function getRoles()
     {
