cd96286e61f6761e6b8908522e7e44545546ef9f
Bella <text_me2@protonmail.com>
2019-02-23 11:36:36 +0100

    code cleaning

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -20,24 +20,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AuthController extends AbstractController
 {
-    public function register(Request $request, UserPasswordEncoderInterface $encoder)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $username = $request->request->get('_username');
-        $password = $request->request->get('_password');
-
-        $user = new User($username);
-        $user->setPassword($encoder->encodePassword($user, $password));
-        $em->persist($user);
-        $em->flush();
-        return new Response(sprintf('User %s successfully created', $user->getUsername()));
-    }
-    public function api()
-    {
-        return new Response(sprintf('Logged in as %s', $this->getUser()->getUsername()));
-    }
-
     /**
      * @Rest\Get("/list_user", name="list_user")
      * @Rest\QueryParam(
@@ -81,7 +63,7 @@ class AuthController extends AbstractController
 
     /**
      * @Rest\Delete(
-     *     path="/user_delete/{id}",
+     *     path="/delete_user/{id}",
      *     name="user_delete",
      *     requirements={"id" = "\d+"}
      *     )
